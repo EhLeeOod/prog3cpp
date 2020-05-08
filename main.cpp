a9a54dee1915c9c896619d549e2b6f988acde0ae
Nobody <urestieliud@gmail.com>
2020-05-08 15:36:29 -0500

    created Circle circular to demo new changes to constructor in functions.h

--- main.cpp
+++ main.cpp
@@ -8,5 +8,10 @@ using namespace std;
 int main()
 {
   Demo demoObject;
+  cout << endl << endl;
+  Circle circular;
+  circular.setRadius(1);
+  cout << circular.calcArea() << "." << endl;
+
   return 0;
 }
