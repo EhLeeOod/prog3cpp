216d745ad6829e5983c7b4d3b53280534ef77569
EhLeeOod <UrestiEliud@gmail.com>
2020-05-20 19:49:57 -0500

    added for loop to demo array input & output

--- main.cpp
+++ main.cpp
@@ -10,43 +10,44 @@ int main()
 {
   const int TEST_INT = 10;
   int hours[TEST_INT];
+  int count = 0;
+  for (count = 0; count < TEST_INT; count++)
+    cin >> hours[count];
 
-  hours[0] = 1;
-  hours[1] = 2;
+  cout << "The hours you entered are: ";
 
-  cout << endl << endl << hours[0] << endl << hours[1];
-
-  cout << "test" << endl;
-
-  Demo demoObject;
-  cout << endl << endl;
-  Demo demoObjectWithParams(17);
-  cout << endl << endl;
-  Circle circular;
-  circular.setRadius(1);
-  cout << circular.calcArea() << "." << endl;
-
-  Carpet purchase;
-
-  double pricePerYd;
-  double length;
-  double width;
-
-
-
-  cout << "Room length in feet: ";
-  cin >> length;
-  cout << "Room width in feet: ";
-  cin >> width;
-  cout << "Carpet price per square yard: ";
-  cin >> pricePerYd;
-
-  purchase.setDimensions(length, width);
-  purchase.setPricePerYd(pricePerYd);
-
-  cout << endl << "The total price of the " << length << " x " << width << " carpet is $" << purchase.getTotalPrice() << "." << endl;
-
-
+  for (count = 0; count < TEST_INT; count++)
+    cout << " " << hours[count];
+  //
+  // Demo demoObject;
+  // cout << endl << endl;
+  // Demo demoObjectWithParams(17);
+  // cout << endl << endl;
+  // Circle circular;
+  // circular.setRadius(1);
+  // cout << circular.calcArea() << "." << endl;
+  //
+  // Carpet purchase;
+  //
+  // double pricePerYd;
+  // double length;
+  // double width;
+  //
+  //
+  //
+  // cout << "Room length in feet: ";
+  // cin >> length;
+  // cout << "Room width in feet: ";
+  // cin >> width;
+  // cout << "Carpet price per square yard: ";
+  // cin >> pricePerYd;
+  //
+  // purchase.setDimensions(length, width);
+  // purchase.setPricePerYd(pricePerYd);
+  //
+  // cout << endl << "The total price of the " << length << " x " << width << " carpet is $" << purchase.getTotalPrice() << "." << endl;
+  //
+  //
 
   return 0;
 }
